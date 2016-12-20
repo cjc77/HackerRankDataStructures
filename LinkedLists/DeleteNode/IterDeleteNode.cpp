95b7c85a90285a8aa07ed300c68deb94ec1b4512
carson <carsonjamescook@gmail.com>
2016-12-20 12:27:59 -0800

    Solved delete node.

---
+++ LinkedLists/DeleteNode/IterDeleteNode.cpp
@@ -0,0 +1,28 @@
+Node * Delete(Node * head, int position)
+{
+    Node * current = head;
+    Node * previous = NULL;
+    int currPos = 0;
+    if(!head) {
+        return head;
+    }
+    if(position == 0) {
+        current = head->next;
+        delete head;
+        head = current;
+    }
+    else {
+        // Find "position" or stop if reached end of list
+        while(current && currPos < position) {
+            previous = current;
+            current = current->next;
+            ++currPos;
+        }
+        // If "position" exists in this list
+        if(currPos == position) {
+            previous->next = current->next;
+            delete current;
+        }
+    }
+    return head;
+}
