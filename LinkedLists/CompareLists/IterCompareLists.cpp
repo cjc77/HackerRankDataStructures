fc5be17c24b9980ceab0d32747d6c48e3c35b0af
carson <carsonjamescook@gmail.com>
2016-12-20 14:27:56 -0800

    Solved compare lists.

---
+++ LinkedLists/CompareLists/IterCompareLists.cpp
@@ -0,0 +1,34 @@
+int CompareLists(Node * headA, Node * headB)
+{
+    Node * currentA = headA;
+    Node * currentB = headB;
+    int match = 1;
+
+    // If either head is empty
+    if(!currentA || !currentB) {
+        // If one is empty and not the other
+        if((!currentA && currentB) || (!currentB && currentA)) {
+            match = 0;
+        }
+        // If they're both empty
+        else {
+            match = 1;
+        }
+    }
+    else {
+        // While both nodes not NULL
+        while(currentA && currentB) {
+            // if data doesn't match
+            if(currentA->data != currentB->data) {
+                match = 0;
+            }
+            currentA = currentA->next;
+            currentB = currentB->next;
+        }
+        // If one is longer
+        if(currentA || currentB) {
+            match = 0;
+        }
+    }
+    return match;
+}
