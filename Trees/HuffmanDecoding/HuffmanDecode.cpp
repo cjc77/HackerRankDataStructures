58e289b1356d930660cf2bf88af196ad22cfe270
cjc77 <carsonjamescook@gmail.com>
2016-12-30 16:42:25 -0800

    Comments.

--- Trees/HuffmanDecoding/HuffmanDecode.cpp
+++ Trees/HuffmanDecoding/HuffmanDecode.cpp
@@ -1,33 +1,41 @@
+/*
+You have to decode an encoded string using the Huffman tree.
 
+You are given pointer to the root of the Huffman tree and a binary
+coded string. You need to print the actual string.
+*/
 
 
 void decode_huff(node * root, string s)
 {
     node * current = root;
     int size = s.size();
-    char zero = '0';
-    char one = '1';
-    int ltr = 0;
+    char ZERO = '0';
+    char ONE = '1';
+    int letter = 0;
+    // Is the darn thing empty?
     if(!root) {
         return;
     }
 
-    while(ltr < size) {
-        if(s[ltr] == zero) {
+    while(letter < size) {
+        if(s[letter] == ZERO) {
             current = current->left;
+            // If we have reached a leaf
             if(!current->left && !current->right) {
                 cout << current->data;
                 current = root;
             }
-            ++ltr;
+            ++letter;
         }
-        else if(s[ltr] == one) {
+        else if(s[letter] == ONE) {
             current = current->right;
+            // If we have reached a leaf
             if(!current->left && !current->right) {
                 cout << current->data;
                 current = root;
             }
-            ++ltr;
+            ++letter;
         }
     }
 }
